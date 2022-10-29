6a5dea7c00ed610e0aa4385e1e1562d5a74fd10c
mbcodes <mbcodes@protonmail.com>
2022-10-28 22:48:23 -0700

    array practice trois point deux

--- arrays/encode_decode_strings.js
+++ arrays/encode_decode_strings.js
@@ -1,29 +1,34 @@
+// design algo
+
 const encode = function (arr) {
-  let result = '';
+  let result = ''; // start with empty string
 
   for (let str of arr) {
+    // loop through each str in array
     result += `${str.length}#${str}`;
+    // to the result empty str, add the length of word + # with the remaining word as encoding
+    // having this delimiter num# will tell us how long the word is and if there's a # used in a string, it won't accidentally delimit it
   }
-
-  return result;
+  return result; // return encoded str
 };
 
 const decode = function (str) {
-  let i, j, length, res;
-  [res, i] = [[], 0];
+  let i, j, length, res; // declare different variables
+  [res, i] = [[], 0]; // destructure arr and reassign res = [], i = 0
 
   while (i < str.length) {
-    j = i;
+    // while i is less than the str length, continue loop
 
+    j = i; // j will look through each letter, so it starts at same position as i
     while (str[j] !== '#') {
+      // while letter str[j] is not a pound sign, keep increasing j
       j += 1;
     }
-
-    length = Number.parseInt(str.slice(i, j));
-    res.push(str.slice(j + 1, j + 1 + length));
-    i = j + 1 + length;
+    length = Number.parseInt(str.slice(i, j)); // length is the str.length including i upto j, but not included j, and parse that into a number
+    res.push(str.slice(j + 1, j + 1 + length)); // push into res arr the string from j + 1 (the letter after #) upto, but not including, j + 1 + length of str determine above
+    i = j + 1 + length; // update i to the end of the word we just pushed
   }
-  return res;
+  return res; // return res arr
 };
 
 // TC: O(N), SP: O(1)
@@ -42,6 +47,3 @@ const decod = function (str) {
   }
   return str.split('@enconde@');
 };
-
-console.log(encod(['lint', 'code', 'love', 'you']));
-console.log(decod('lint@enconde@code@enconde@love@enconde@you'));
--- arrays/longest_consecutive_sequence.js
+++ arrays/longest_consecutive_sequence.js
@@ -1,1 +1,23 @@
-const longestConsecutive = function (nums) {};
+const longestConsecutive = function (nums) {
+  const set = new Set(nums); // to keeps numbers from repeating
+  let longest = 0; // keep track of longest sequence
+
+  for (let num of nums) {
+    // loop over nums array
+    if (!set.has(num - 1)) {
+      // check if num has a left neighbor, it it doesn't that means it's the start of a sequence
+      let count = 0; // initial length of sequence
+      while (set.has(num)) {
+        // after starting a sequence, keep checking if our number exists in the set, if it does, keep going
+        num++; // check for the next number in sequence
+        count++; // increment count of sequence
+      }
+      longest = Math.max(longest, count); // which is greater, longest or count, then reassign that to longest
+    }
+  }
+  return longest; // return longest
+};
+
+// TC: O(N), SC: O(N)
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
