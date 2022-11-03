c6e27b74e2813e025710de3a10d1f246c8331d74
mbcodes <mbcodes@protonmail.com>
2022-11-02 18:35:42 -0700

    binary search practice

--- binary_search/find_min_rotated_sorted_arr.js
+++ binary_search/find_min_rotated_sorted_arr.js
@@ -1,19 +1,27 @@
 const findMin = function (nums) {
   let left = 0,
     right = nums.length - 1,
-    min = nums[0],
-    mid;
+    min = nums[0], // in regular BS, first element is the smallest, for this problem, use first element as min to start with
+    midpoint;
 
   while (left <= right) {
     if (nums[left] < nums[right]) {
+      // this is for when a subarray is already sorted
       min = Math.min(nums[left], min);
       break;
     }
 
-    mid = Math.floor(left + (right - left) / 2);
-    min = Math.min(min, nums[mid]);
-    if (nums[mid] >= nums[left]) left = mid + 1;
-    else right = mid - 1;
+    midpoint = Math.floor(left + (right - left) / 2);
+    min = Math.min(min, nums[midpoint]);
+    if (nums[midpoint] >= nums[left]) {
+      // if nums[mid] is greater than nums[left], then search right portion of sorted arr
+      left = midpoint + 1;
+    } else {
+      // otherwise, search left side of sorted arr
+      right = midpoint - 1;
+    }
   }
   return min;
 };
+
+// TC: O(log n); SC: O(1)
--- binary_search/search_rotated_sorted_arr.js
+++ binary_search/search_rotated_sorted_arr.js
@@ -2,39 +2,41 @@ const search = function (nums, target) {
   if (!nums.length) return -1; // if no arr exists, return -1
 
   // binary search pointers
-  let min = 0,
-    max = nums.length - 1,
+  let left = 0,
+    right = nums.length - 1,
     midpoint;
 
-  while (min <= max) {
-    // while min less than or equal to max, then...
-    midpoint = Math.floor(min + (max - min) / 2); // find midpoint
+  while (left <= right) {
+    // while left less than or equal to right, then...
+    midpoint = Math.floor(left + (right - left) / 2); // find midpoint
 
     if (nums[midpoint] === target) {
       // if nums[midpoint] equals the target num then return midpoint/idx
       return midpoint;
     }
 
-    //check if number is in left part sorted array
-    if (nums[min] <= nums[midpoint]) {
-      // it's on the left bc nums[min] is less than nums[midpoint]
-      if (nums[min] <= target && target < nums[midpoint]) {
-        // if nums[min] is less than target, and target is less tha midpoint, change max to be smaller than midpoint
-        max = midpoint - 1;
+    //check if number is in left sorted array
+    if (nums[left] <= nums[midpoint]) {
+      // it's on the left bc nums[left] is less than nums[midpoint]
+      if (nums[left] <= target && target < nums[midpoint]) {
+        // if true, target is in the left side
+        right = midpoint - 1;
       } else {
-        // otherwise change min to be bigger than midpoint
-        min = midpoint + 1;
+        // target is in the right
+        left = midpoint + 1;
       }
     } else {
-      // if it's not in the left (if), then it's in the right (else)
-      if (nums[midpoint] < target && target <= nums[max]) {
-        min = midpoint + 1;
+      // if it's not in the left (if), then it's in the right (else) sorted array
+      if (nums[midpoint] < target && target <= nums[right]) {
+        // target is in the right;
+        left = midpoint + 1;
       } else {
-        max = midpoint - 1;
+        // target is in the left
+        right = midpoint - 1;
       }
     }
   }
-  return -1;
+  return -1; // target is not in the array
 };
 
 // TC: O(log n); SC: O(1)
